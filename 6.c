9feff6f8657e948d534856bbdd8bd1241b835eda
saurabh kumar <kumarsaurabh7449@gmail.com>
2026-05-29 11:55:36 +0530

    Added DAY2 assignment

---
+++ 6.c
@@ -0,0 +1,21 @@
+//Write a program to reverse a number
+
+#include <stdio.h>
+int main()
+{
+    int num, reverse = 0, digit;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    while (num != 0)
+    {
+        digit = num % 10;             
+        reverse = reverse * 10 + digit;  
+        num = num / 10;                  
+    }
+
+    printf("Reverse number = %d", reverse);
+
+    return 0;
+}
---
+++ DAY2/5.c
@@ -0,0 +1,22 @@
+// Write a program to find the sum of digits of a numbers
+
+#include <stdio.h>
+int main()
+
+{
+    int n , sum = 0, digit;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    while (n != 0)
+    {
+        digit = n % 10;  
+        sum = sum + digit;
+        n = n / 10;    
+    }
+
+    printf("Sum of digits = %d", sum);
+
+    return 0;
+}
