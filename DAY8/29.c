14d3686879d1cba2526dcc16440ed326f1ec9bfc
saurabh kumar <kumarsaurabh7449@gmail.com>
2026-06-09 13:21:20 +0530

    Added DAY8 Assignment

---
+++ DAY8/29.c
@@ -0,0 +1,21 @@
+//Write a program to print the half pyramid 
+
+#include<stdio.h>
+int main()
+{
+    int i,j,n ;
+
+    for(i =1;i<=6;i++)
+
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+
+    }
+    return 0;
+
+
+}
---
+++ DAY8/31.c
@@ -0,0 +1,25 @@
+//Write a program to print the character triangle
+// A 
+// AB 
+// ABC 
+// ABCD 
+// ABCDE 
+
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    char ch='A';
+
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("%c",'A'+j-1);
+            
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
