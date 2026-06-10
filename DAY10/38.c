9e4f8ce6d99d6a3fb154264d366a4dcf895522a9
saurabh kumar <kumarsaurabh7449@gmail.com>
2026-06-10 15:57:58 +0530

    Added DAY10 Assignment

---
+++ DAY10/38.c
@@ -0,0 +1,27 @@
+//Write a program to print the reverse star pyramid
+// ********* 
+//  ******* 
+//   ***** 
+//    *** 
+//     * 
+
+#include<stdio.h>
+int main()
+{
+    int i,j;
+
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=i-1;j++)
+        {
+            printf(" ");
+        }
+        for(j=1;j<=2*(5-i)+1;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
---
+++ DAY10/40.c
@@ -0,0 +1,34 @@
+//Write a program to print the character pyramid
+//     A 
+//    ABA 
+//   ABCBA 
+//  ABCDCBA 
+// ABCDEDCBA
+
+
+#include<stdio.h>
+int main()
+{
+    int i , j ;
+    
+
+    for (i=1 ; i<=5 ; i++)
+    {
+        for(j=1; j<=5-i;j++)
+        {
+            printf(" ");
+        }
+        for(j=1 ;j<=i ; j++)
+        {
+            printf("%c",'A'+j-1);
+            
+        }
+        for(j=i-1; j>=1; j--)
+        {
+            printf("%c",'A'+j-1);
+        }
+        printf("\n");
+    }
+    return 0;
+
+}
