4bd99ec6f0600e09afc07a5bab256140965003d7
saurabh kumar <kumarsaurabh7449@gmail.com>
2026-06-13 16:36:19 +0530

    Added DAY 13 Assignment

---
+++ DAY13/49.c
@@ -0,0 +1,23 @@
+//Write a program to Input and display array.
+
+#include<stdio.h>
+int main()
+{
+    int arr[10];
+
+    for(int i = 0 ; i< 10 ; i++)
+    {
+        printf("Enter the array elements %d:",i+1);
+        scanf("%d",&arr[i]);
+
+
+    }
+
+    printf("The elents of an array are:");
+    for(int i=0 ; i < 10 ; i++)
+    {
+        printf("%d ",arr[i]);
+
+    }
+    return 0;
+}
